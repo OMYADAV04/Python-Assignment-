79839278e35121304a2bf71335d8def97ae44a2e
OMYADAV04 <136441752+OMYADAV04@users.noreply.github.com>
2023-07-17 18:59:18 +0530

    Add files via upload

---
+++ Assignment9_2.py
@@ -0,0 +1,14 @@
+from Assignment9 import MathOperations, StringOperations
+
+# Perform math operations
+print("Math Operations:")
+print(MathOperations.add(5, 3))
+print(MathOperations.subtract(10, 4))
+print(MathOperations.multiply(6, 7))
+print(MathOperations.divide(15, 3))
+
+# Perform string operations
+print("\nString Operations:")
+print(StringOperations.concatenate("Hello", " World!"))
+print(StringOperations.count_characters("OpenAI"))
+print(StringOperations.reverse("Python"))
